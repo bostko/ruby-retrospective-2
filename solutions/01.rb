2179e8ff673874dc554b532f8a4f3be313e3ec59
Valentin Aitken <valentin.aitken@shtrak.bg>
2012-12-29 19:30:30 +0200

    Най-доброто възможно решение на първа задача

--- solutions/01.rb
+++ solutions/01.rb
@@ -2,63 +2,38 @@
 
 class Integer
   def prime_divisors
-    factors = []
-    d = 2
+    2.upto(abs).select { |n| remainder(n).zero? and n.prime? }
+  end
 
-    tmp = self
-    while tmp > 1
-      while tmp % d == 0
-        factors << d unless factors.last == d
-        tmp /= d
-      end
-      d += 1
-    end
-    factors
+  protected
+  def prime?
+    2.upto(pred).all? { |n| remainder(n).nonzero? }
   end
 end
-
 
 class Range
   def fizzbuzz
-    res = []
-    each do |x|
-      if x % 3 == 0 and x % 5 == 0
-        res << :fizzbuzz
-      elsif x % 3 == 0
-        res << :fizz
-      elsif x % 5 == 0
-        res << :buzz
-      else
-        res << x
+    map do |n|
+      if    n % 15 == 0 then :fizzbuzz
+      elsif n % 3  == 0 then :fizz
+      elsif n % 5  == 0 then :buzz
+      else n
       end
     end
-    res
   end
 end
-
 
 class Hash
   def group_values
-    new_hash = Hash.new { |h,k| h[k] = [] }
-    each do |k, v|
-      new_hash[v] << k
+    each_with_object({}) do |(key, value), result|
+      result[value] ||= []
+      result[value] << key
     end
-    new_hash
   end
 end
 
-
 class Array
   def densities
-    freq_tbl = Hash.new(0)
-    each do |x|
-      freq_tbl[x] += 1
-    end
-
-    res = []
-    each do |x|
-      res << freq_tbl[x]
-    end
-    res
+    map { |item| count item }
   end
 end
